97f80fa3493e9fd857ff0d46a3e5dd73bf0d9ea5
thebigdavec <daverbcollison@gmail.com>
2020-08-09 16:42:17 +0100

    At the end of the crash course

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+// const logger = require('./middleware/logger');
+const members = require('./Members');
+
+const app = express();
+
+// Initialise middleware
+// app.use(logger);
+// Handlebars middleware
+app.engine('handlebars', exphbs({defaultLayour: 'main'}));
+app.set('view engine', 'handlebars');
+// Body Parser middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false})); 
+
+// Homepage route
+app.get('/', (req, res) => res.render('index', { title: 'Member App', members }));
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Members API routes
+app.use('/api/members', require('./routes/api/members'));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
